e24031d793d2fbd90219f089073c53da5b05eb64
Shreyansh Khandelwal <khandelwal.happy.hk@gmail.com>
2024-07-02 01:14:06 +0530

    app crash bug fixed (#1)
    * app crash bug fixed
    
    * env file added for api
    
    * minor changes
    
    * app version changed
    
    * minor changes in gitIgnore files
    
    * Update README.md
    
    * minor changes
    
    ---------
    
    Co-authored-by: Mahesh kandula <93394906+mahi6148@users.noreply.github.com>

--- lib/Utilities/bottombar.dart
+++ lib/Utilities/bottombar.dart
@@ -2,14 +2,11 @@ import 'dart:ui';
 
 import 'package:aarush/Common/common_controller.dart';
 import 'package:aarush/Data/bottomIndexData.dart';
-import 'package:aarush/Model/Events/event_list_model.dart';
-import 'package:aarush/Screens/Events/events_screen.dart';
 import 'package:aarush/Screens/Home/home_screen.dart';
 import 'package:aarush/Screens/Profile/profilepage.dart';
 import 'package:aarush/Screens/Tickets/myEvents.dart';
 import 'package:aarush/Themes/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class AaruushBottomBar extends GetView<CommonController> {
@@ -18,13 +15,13 @@ class AaruushBottomBar extends GetView<CommonController> {
     required this.bottomIndex,
   });
   final int bottomIndex;
+
   @override
   Widget build(BuildContext context) {
-    controller.changeBottomBarIndex(bottomIndex);
     return ClipRRect(
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: BottomAppBar(
+        child: Obx(() => BottomAppBar(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           elevation: 0,
           color: Colors.transparent,
@@ -32,47 +29,53 @@ class AaruushBottomBar extends GetView<CommonController> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               _bottomIcons(
-                  isSelected:
-                      controller.bottomBarIndex.value == BottomIndexData.HOME,
-                  onTap: () => {Get.to(() => const HomeScreen())},
+                  isSelected: controller.bottomBarIndex.value ==
+                      BottomIndexData.HOME,
+                  onTap: () => navigateTo(() => const HomeScreen(), BottomIndexData.HOME),
                   icon: Icons.home_rounded),
               _bottomIcons(
                   isSelected: controller.bottomBarIndex.value ==
                       BottomIndexData.TICKETS,
-                  onTap: () => {Get.to(() => MyEvents())},
+                  onTap: () => navigateTo(() => MyEvents(), BottomIndexData.TICKETS),
                   icon: Icons.confirmation_number_rounded),
               _bottomIcons(
                   isSelected: controller.bottomBarIndex.value ==
                       BottomIndexData.PROFILE,
-                  onTap: () =>
-                      {debugPrint("Profile"), Get.to(() => ProfileScreen())},
+                  onTap: () => navigateTo(() => ProfileScreen(), BottomIndexData.PROFILE),
                   icon: Icons.person_rounded),
             ],
           ),
-        ),
+        )),
       ),
     );
   }
 
+  void navigateTo(Widget Function() screen, int index) {
+    try {
+      controller.changeBottomBarIndex(index);
+      Get.offAll(screen);
+    } catch (e) {
+      debugPrint("Navigation error: $e");
+    }
+  }
+
   Widget _bottomIcons(
       {required VoidCallback onTap,
-      required IconData icon,
-      bool isSelected = false}) {
-    if (isSelected) {
-      return Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Get.theme.curveBG.withOpacity(0.5),
-          ),
-          child: IconButton(
-            onPressed: onTap,
-            icon: Icon(icon),
-            color: Colors.orange,
-            iconSize: 25,
-          ));
-    }
-
-    return IconButton(
+        required IconData icon,
+        bool isSelected = false}) {
+    return isSelected
+        ? Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Get.theme.curveBG.withOpacity(0.5),
+        ),
+        child: IconButton(
+          onPressed: onTap,
+          icon: Icon(icon),
+          color: Colors.orange,
+          iconSize: 25,
+        ))
+        : IconButton(
       onPressed: onTap,
       icon: Icon(icon),
       color: Colors.white,
